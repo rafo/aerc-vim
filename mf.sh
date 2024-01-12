0133b8cac0c17e046cc4d6a090f159dd452a36c9
Rafo <rafael.tomic@gmail.com>
2024-01-12 15:30:53 +0100

    Delete obsolete tests.

--- mf.sh
+++ mf.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Überprüfen, ob eine Eingabedatei angegeben wurde
-#if [ "$#" -ne 1 ]; then
-#    echo "Usage: $0 <path_to_email_file>"
-#    exit 1
-#fi
-
-# Eingabedatei lesen und E-Mail extrahieren
-# awk -F"<|>" '/From: /{print $2}' "$1" >> extracted_emails.txt
-
-#!/bin/bash
-
-# E-Mail-Daten von der Standardeingabe (stdin) lesen und E-Mail-Adresse extrahieren
-awk -F"<|>" '/From: /{print $2}' >> /Users/rafael/.config/imapfilter/mf.txt
+# awk -F to specify the field separator (in this case, < or >) and print the second field (the email address) to a file where imapfilter can read it.
+awAk -F"<|>" '/From: /{print $2}' >>~/.config/imapfilter/mf.txt
