a6f04d51fd9507fff4a41354f14ddff2f33d618b
Rafo <rafael.tomic@gmail.com>
2024-08-22 20:55:20 +0200

    small changes

--- aerc.sh
+++ aerc.sh
@@ -2,4 +2,5 @@
 # Source https://aerc-mail.org/
 # Aerc is an email client for your terminal.
 
-alias _a='aerc' # Minimalistic E-Mail client
+alias ,a='aerc'    # Minimalistic E-Mail client
+alias ae_rc='aerc' # Minimalistic E-Mail client
